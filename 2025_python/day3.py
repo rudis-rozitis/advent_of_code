43f663bbca0bed9a3e5d8bc4d74e152065877884
Rudis Rozitis <rudisrozitis@Rudiss-MacBook-Air.local>
2026-08-14 22:18:19 +0300

    day 3, part1 solution

--- 2025_python/day3.py
+++ 2025_python/day3.py
@@ -1,4 +1,4 @@
-# ----------------- Part1, day2 ----------------
+# ----------------- Part1, day3 ----------------
 
 import os
 
@@ -8,39 +8,30 @@ script_dir = os.path.dirname(__file__)
 
 
 def find_both_highest(number_list: list):
-    highest = 0
-    h_i = 0
-    second_highest = 0
-    s_i = 0
+    highest = -1
+    second_highest = -1
     for i, number in enumerate(number_list):
 
-        # First 2 numbers in bank
-        if highest == 0:
+        if number > highest and i != len(number_list) - 1:
             highest = number
-            h_i = i
-            continue
-        if second_highest == 0:
+            second_highest = -1
+        elif number > second_highest:
             second_highest = number
-            s_i = i
-            continue
+        elif i == len(number_list) - 1 and number > highest:
+            highest = second_highest
+            second_highest = number
+            
 
-        if number > highest:
-            if i != len(number_list) - 1:
-                second_highest = 0
-                s_i = 0
-            else:
-                second_highest = highest
-                s_i = h_i
-            highest = number
-            h_i = i
-
-    return (highest, h_i), (second_highest, s_i)
+    return highest, second_highest
 
 
-with open(f"{script_dir}/testinput.txt", "r") as file:
+with open(f"{script_dir}/input.txt", "r") as file:
     lines = file.readlines()
 
+sum_of_joltages = 0
 for line in lines:
 
     a, b = find_both_highest(list(map(int, list(str(line.strip())))))
-    print(f"{a[0]}{b[0]}")
+    #print(f"{a}{b}")
+    sum_of_joltages += int(f"{a}{b}")
+print(sum_of_joltages)
